459016be749334578885645db28da7de8e6889f4
raineryue <jinlei_yue@sina.com>
2015-12-18 16:03:23 +0800

    Initial Commit

---
+++ 20151218001-UITableViewController-QQMessageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20151218001-UITableViewController-QQMessageView
+//
+//  Created by Rainer on 15/12/18.
+//  Copyright © 2015年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
